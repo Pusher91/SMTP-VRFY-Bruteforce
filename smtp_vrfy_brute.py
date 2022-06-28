37822382d2350574737d991c321cccf896796668
Pusher91 <76879126+Pusher91@users.noreply.github.com>
2022-06-28 16:26:07 -0500

    Update smtp_vrfy_brute.py

--- smtp_vrfy_brute.py
+++ smtp_vrfy_brute.py
@@ -9,31 +9,40 @@ if len(sys.argv)!=3:
 
 else:
     target = sys.argv[1]
+    # Define user_list variable here so the script will pick up where left upon reconnect.
     user_list = open(sys.argv[2], "r")
     
-    print(f"\nConnecting to {sys.argv[1]}")
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((target,25))
-    response = s.recv(2048)
-    
-    print(f"Connect response: {response.decode()}")
+    with open(sys.argv[2], "r") as fp:
+        for count, line in enumerate(fp):
+            pass
+    user_list_lines = count+1
 
     count = 0
-    for user in user_list:
-        try:
-            vrfy_attempt = f"VRFY {user}"
-            count += 1
+    while user_list_lines:    
 
-            print(f"{count} | {vrfy_attempt.strip()}               ", end="\r")
-            s.send(vrfy_attempt.encode())
-            response = s.recv(2048)
+        print(f"\nLines remaining in user list: {user_list_lines}")
+        
+        print(f"Connecting to {sys.argv[1]}")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target,25))
+        response = s.recv(2048)
+        print(f"Connect response: {response.decode()}")
 
-            if re.search(r"^252", response.decode()):
-                print(f"+ Verified user: {user}", end="")
+        for user in user_list:
+            user_list_lines -= 1
+            try:
+                vrfy_attempt = f"VRFY {user}"
+                count += 1
 
-        except (ConnectionResetError, BrokenPipeError) as e:
-            print(f"\n\n* ConnectionResetError encountered - script unable to complete")
-            raise SystemExit()
+                print(f"{count} | {vrfy_attempt.strip()}               ", end="\r")
+                s.send(vrfy_attempt.encode())
+                response = s.recv(2048)
+
+                if re.search(r"^252", response.decode()):
+                    print(f"+ Verified user: {user}", end="")
+
+            except (ConnectionResetError, BrokenPipeError) as e:
+                print("\n\n* ConnectionResetError encountered - Reconnecting to target.", end="\r")
+                break
 
     print("\nBruteforce Complete.")

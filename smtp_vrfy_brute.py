fda84ce9452a9a1b9225d0242bd8c6cbcb4abd6a
Pusher91 <76879126+Pusher91@users.noreply.github.com>
2022-06-28 14:20:18 -0500

    Create smtp_vrfy_brute.py

---
+++ smtp_vrfy_brute.py
@@ -0,0 +1,30 @@
+import socket
+import os
+import sys
+import re
+
+if len(sys.argv)!=3:
+    print("script.py <Target> <Username List>")
+    print("Example: ./script.py 192.168.1.1 users.txt")
+
+else:
+    target = sys.argv[1]
+    user_list = open(sys.argv[2], "r")
+    
+    print(f"\nConnecting to {sys.argv[1]}")
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((target,25))
+    response = s.recv(2048)
+    
+    print(f"Connect response: {response.decode()}")
+
+    for user in user_list:
+        vrfy_attempt = (f"VRFY " + user).encode()
+        s.send((f"VRFY " + user).encode())
+        response = s.recv(2048)
+
+        if re.search(r"^252", response.decode()):
+            print(f"Verified user: {response.decode()}", end="")
+
+    print("\nBruteforce Complete.")
